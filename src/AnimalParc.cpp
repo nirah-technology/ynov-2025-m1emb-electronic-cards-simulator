0b789708655d2111307105bd29c99bd705cd3cb2
Nicolas Metivier <nicolas.a.metivier@gmail.com>
2025-11-25 16:30:45 +0100

    Add Interface, Abstract classes, concret classes

--- src/AnimalParc.cpp
+++ src/AnimalParc.cpp
@@ -1,41 +1,126 @@
 #include <string>
+#include <iostream>
 
-class Animal {
-
-private:
-    // Champs Statiques
-    
-    // Propriétés
-    unsigned int birth_date;
-    std::string breed;
-    
-protected:
-Animal() {
-    this->total_animals_created++;
-}
-
-public:
-    static int total_animals_created;
-    void eat() {};        
+struct GpsCoordinates {
+    double longitude;
+    double latitude;
+    double altitude;
 };
-    
-class Cat : public Animal {
+
+// INTERFACES - FONCTIONNALITES
+class IAbleToMove {
+    public:
+    virtual ~IAbleToMove() = default;
+
+    virtual void move(const double longitude, const double latitude, const double altitude) = 0;
+    virtual GpsCoordinates retrieve_location() = 0;
+    protected:
+        IAbleToMove() = default;
+};
+
+class IAbleToCommunicate {
+    public:
+    virtual ~IAbleToCommunicate() = default;
+
+    virtual void communicate() = 0;
+    protected:
+        IAbleToCommunicate() = default;
+};
+
+class IAbleToMeow {
+    public:
+    virtual ~IAbleToMeow() = default;
+
+    virtual void meow() = 0;
+    protected:
+        IAbleToMeow() = default;
+};
+
+class IAbleToBark {
+    public:
+    virtual ~IAbleToBark() = default;
+
+    virtual void bark() = 0;
+    protected:
+        IAbleToBark() = default;
+};
+
+class IAbleToLive {
+    public:
+    virtual ~IAbleToLive() = default;
+
+    virtual void birth(unsigned int birth_date) = 0;
+    virtual void death(unsigned int death_date) = 0;
+    protected:
+        IAbleToLive() = default;
+};
+
+class Animal : public IAbleToLive, public IAbleToMove, public IAbleToCommunicate {
     private:
-    static int total_cats_created;
-public:
-    Cat() {
-        this->total_cats_created++;
-    }
+        unsigned int birth_date;
+        unsigned int death_date;
+        GpsCoordinates location; 
+        
+    protected:
+        std::string species;
+        std::string breed;
+        std::string name;
+        
+        
+        
+    public:
+        Animal() = default;
+        ~Animal() = default;
 
-    static void sauvegarder() {
-    }
-    void miauler();
-    void courir() {
+        void birth(unsigned int birth_date) override {
+            this->birth_date = birth_date;
+        }
 
-    };
+        void death(unsigned int death_date) override {
+            this->death_date = death_date;
+        }
+
+        void move(const double longitude, const double latitude, const double altitude) {
+            this->location.altitude = altitude;
+            this->location.longitude = longitude;
+            this->location.latitude = latitude;
+        }
+
+        GpsCoordinates retrieve_location() {
+            return this->location;
+        }
 };
 
-class Dog : public Animal {
-    
+class Cat : public Animal, public IAbleToMeow {
+    public:
+        Cat(std::string breed, std::string name) {
+            this->species = "Cat";
+            this->breed = breed;
+            this->name = name;
+        }
+
+        void meow() override {
+            std::cout << "Miou" << std::endl;
+        }
+
+        void communicate() override {
+            this->meow();
+        }
 };
 
+class Dog : public Animal, public IAbleToBark {
+    public:
+        Dog(std::string breed, std::string name) {
+            this->species = "Dog";
+            this->breed = breed;
+            this->name = name;
+        }
+
+        void bark() override {
+            std::cout << "Ouaf" << std::endl;
+        }
+
+        void communicate() override {
+            this->bark();
+        }
+};
--- src/Program.cpp
+++ src/Program.cpp
@@ -2,57 +2,24 @@
 #include <string>
 
 #include "AnimalParc.cpp"
-#include "GameLoader.cpp"
-
-class ElectronicCard {
-
-private:
-    std::string manufacturer;
-    std::string model;
-    unsigned int memory_in_megabits;
-    unsigned short cpu_cores_count;
-    double cpu_frequency;
-
-public:
-
-    ElectronicCard() {
-        std::cout << "Instanciation d'une carte électronique." << std::endl;
-    }
-    
-    ~ElectronicCard() {
-        std::cout << "Destruction d'une carte électronique." << std::endl;
-
-    }
-
-    std::string get_manufacturer() {
-        return this->manufacturer;
-    }
-    std::string get_model() {
-        return this->model;
-    }
-    unsigned int get_memory_in_megabits() {
-        return this->memory_in_megabits;
-    }
-    unsigned int get_cpu_cores_count() {
-        return this->cpu_cores_count;
-    }
-    void set_cpu_cores_count(unsigned int new_cpu_cores_count) {
-        this->cpu_cores_count = new_cpu_cores_count;
-    }
-
-};
-
-Animal* generate_animal() {
-    return new Dog();
-};
 
 int main() {
+    // Animal* pet = new Dog("Yorkshire", "Yuma");
+    // pet->communicate();
 
-    Animal* pet = generate_animal();
-    std::cout << Animal::total_animals_created;
+    // Dog* pet = new Dog("Yorkshire", "Yuma");
+    // pet->communicate();
+    // pet->
 
-    
-    GameLoader::backup();
+    // IAbleToBark* pet = new Dog("Yorkshire", "Yuma");
+    // pet->;
+
+    // IAbleToMeow* pet = new Dog("Yorkshire", "Yuma");
+    // pet->communicate();
+
+
+    IAbleToCommunicate* pet = new Dog("Yorkshire", "Yuma");
+    pet->communicate();
 
     return 0;
 }
